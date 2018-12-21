9feedead47a7d684e5428a9d738307cb0535e552
wyc0807 <wangyichen5151@gmail.com>
2018-12-20 22:50:28 -0700

    delete redundant codes

--- codes/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-1. statistics_basic.ipynb is the Jupyter Notebook that does statistics about the dataset and extracts all the basic features
-2. Statistics_ComplexFeatures.ipynb is the Jupyter Notebook that extracts all the complex features from basic features. 
-   Also the script includes the way to compare distributions of these complex features between filtered/non-filtered reviews.
-3. classification.ipynb is the Jupyter Notebook that does all the classification tasks (including feature scaling and grid search) except for Xgboost
-4. xgb_classification&featureranking.py does Xgboost classification as well as feature ranking
--- codes/xgb_classification&featureranking.py
+++ codes/xgb_classification&featureranking.py
@@ -17,20 +17,7 @@ from sklearn import cross_validation,metrics
 from sklearn import linear_model
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix, roc_auc_score, classification_report, roc_curve, auc, accuracy_score  
-'''
-conn = pymysql.connect(host='192.168.1.131',user='yichen', password='0000', database='yelp_db', charset='utf8')
-cur = conn.cursor()
 
-#query="select id as user_id, review_count,yelping_since,useful,funny,cool,fans,average_stars, compliment_hot+ compliment_more+ compliment_profile+ compliment_cute+ compliment_list+ compliment_note+ compliment_plain+compliment_cool+compliment_funny+compliment_writer+compliment_photos as sum_compliment from user"
-query='select id as review_id,business_id,user_id,stars,date,useful,funny,cool from review'
-cur.execute(query)
-#cur.execute('commit')
-t=cur.fetchall()
-#user_info_50000=pd.DataFrame(list(t), columns = [i[0] for i in cur.description])
-review_info_notext=pd.DataFrame(list(t), columns = [i[0] for i in cur.description])
-print('done')
-review_info_notext.to_csv('E:/DATA/DATA in CU Boulder/CSCI 5622/review_info_notext.csv')
-'''
 df=pd.read_csv('info_addFeatures_new_v2.csv',encoding='gb18030')
 
 train, test = train_test_split(df, test_size=0.2)
@@ -117,60 +104,6 @@ y_test_Xgb = y_test['a']
 xgb_model = xgb.XGBClassifier(learning_rate = 0.3, max_depth = 6, min_child_weight = 1, gamma = 0.1).fit(x_train_Xgb, y_train_Xgb)
 pre_Xgb = xgb_model.predict(x_test_Xgb)
 print(pre_Xgb)
-###SVM returns only label
-# model = svm.SVC(kernel = 'linear', decision_function_shape = 'ovr')
-### RandomForest could return the proba
-# model = RandomForestClassifier(n_estimators=70, min_samples_split=10, min_samples_leaf=10,max_depth =22,max_features='sqrt' ,random_state=10,class_weight = 'balanced')
-###GradientBoosting return the proba
-# model = GradientBoostingClassifier()
-###adaboost could return the proba
-# model = AdaBoostClassifier( base_estimator = RandomForestClassifier(n_estimators=70, min_samples_split=10, min_samples_leaf=10,max_depth =22,max_features='sqrt' ,random_state=10,class_weight = 'balanced'),                     #algorithm="SAMME",
-                 #n_estimators=50, learning_rate=0.1)
-    ###grid search for Xgb
-#find eta
-#print('find learning_rate')
-#param_test1 = { 'learning_rate':[0.001,0.01,0.05,0.1,0.3,0.5,1.0,2.0,10.0]}
-#gsearch1 = GridSearchCV(estimator = xgb.XGBClassifier(max_depth = 6, min_child_weight = 1, gamma = 0),
-               #param_grid = param_test1, scoring='accuracy',cv=5)
-#gsearch1.fit(x_train_Xgb, y_train_Xgb)
-#print(gsearch1.grid_scores_, gsearch1.best_params_, gsearch1.best_score_)
- #find min_child_weight
-'''
-print('find min_child_weight')
-param_test2 = { 'min_child_weight':[0.1,0.5,1.0,1.5,2.0]}
-gsearch2 = GridSearchCV(estimator = xgb.XGBClassifier(learning_rate = 0.3, max_depth = 6, gamma = 0),
-               param_grid = param_test2, scoring='roc_auc',cv=5)
-gsearch2.fit(x_train, y_train)
-print(gsearch2.grid_scores_, gsearch2.best_params_, gsearch2.best_score_)
-'''    #find max_depth
-'''
-print('find max_depth')
-param_test2 = { 'max_depth':[1,3,6,8,10]}
-gsearch2 = GridSearchCV(estimator = xgb.XGBClassifier(learning_rate = 0.3, min_child_weight = 1, gamma = 0),
-               param_grid = param_test2, scoring='roc_auc',cv=5)
-gsearch2.fit(x_train, y_train)
-print(gsearch2.grid_scores_, gsearch2.best_params_, gsearch2.best_score_)
-'''    #find gamma
-'''
-print('find gamma')
-param_test2 = { 'gamma':[0,0.1,0.5,1.0]}
-gsearch2 = GridSearchCV(estimator = xgb.XGBClassifier(learning_rate = 0.3, min_child_weight = 1, max_depth = 6, gamma = 0),
-               param_grid = param_test2, scoring='roc_auc',cv=5)
-gsearch2.fit(x_train, y_train)
-print(gsearch2.grid_scores_, gsearch2.best_params_, gsearch2.best_score_)
-'''    # confusion matrix
-'''confusion = confusion_matrix(y_test, predictions)
-print("\t" + "\t".join(str(x) for x in range(0, 2)))
-print("".join(["-"] * 50))
-for ii in range(0, 2):
-    jj = ii
-    print("%i:\t" % jj + "\t".join(str(confusion[ii][x]) for x in range(0, 2)))    print(pre)
-'''
-#print(f1_score(y_test, predictions))
-#print(precision_score(y_test, predictions))
-#print(recall_score(y_test, predictions))
-# print(roc_auc_score(y_test, predictions))
-# print(classification_report(y_test, predictions))
 
 t=accuracy_score(y_test,pre_Xgb)
 pred_proba_xgb=xgb_model.predict_proba(x_test)
